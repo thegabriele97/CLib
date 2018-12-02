224fb3060735f674a3b6b5643a2f104ca905f309
Salvatore Gabriele La Greca <gabriele.lagreca@gmail.com>
2018-12-02 12:57:16 +0100

    light version of waitsig

--- source/waitsig/src/waitsig.c
+++ source/waitsig/src/waitsig.c
@@ -1,63 +1,22 @@
 #include "../include/waitsig.h"
 #include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
 #include <signal.h>
 #include <stdbool.h>
-#include <fcntl.h>
-#include <stdio.h>
-#include <stdlib.h>
 
-#define CODE_GO 0x1
-
-pid_t child_pid;
+bool is_waiting;
 
 void HDL_SIG(int sig) {
-    kill(child_pid, SIGUSR1);
-}
-
-void HDL_SIGUSR1(int sig) {
-    raise(SIGKILL);
+    is_waiting = false;
 }
 
 void waitsig(int sig) {
-    int _pipe_p[2], _pipe_c[2];
-    char code;
+    
+    signal(sig, HDL_SIG);
+    is_waiting = true;
 
-    pipe(_pipe_p);
-    pipe(_pipe_c);
-
-    if (child_pid = fork()) {
-        close(_pipe_p[0]);
-        close(_pipe_c[1]);
-
-        signal(sig, HDL_SIG);
-        code = CODE_GO;
-        write(_pipe_p[1], &code, 0x1);
-
-        while(read(_pipe_c[0], &code, 0x1) > 0) {
-            break;
-        }
-
-        wait((int *)0);
-        signal(sig, SIG_DFL);
-    } else {
-
-        close(_pipe_c[0]);
-        close(_pipe_p[1]);
-
-        signal(SIGUSR1, HDL_SIGUSR1);
-        code = CODE_GO;
-        write(_pipe_c[1], &code, 0x1);
-
-        while(read(_pipe_p[0], &code, 0x1) > 0) {
-            break;
-        }
-
-        while(true) {
-            pause();
-        }
-
-        exit(0);
+    while(is_waiting) {
+        pause();
     }
+    
+    signal(sig, SIG_DFL);
 }
